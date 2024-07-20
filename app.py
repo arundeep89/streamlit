fc46624e91181c64ae41e05ddfccf38edf6ef926
Arun Pillai <arundeepak89@gmail.com>
2024-07-20 17:59:02 -0400

    test connection to GBQ

--- app.py
+++ app.py
@@ -1,5 +1,30 @@
 import streamlit as st
 import pandas as pd
+from google.oauth2 import service_account
+from google.cloud import bigquery
+
+# Create API client.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+client = bigquery.Client(credentials=credentials)
+
+# Perform query.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def run_query(query):
+    query_job = client.query(query)
+    rows_raw = query_job.result()
+    # Convert to list of dicts. Required for st.cache_data to hash the return value.
+    rows = [dict(row) for row in rows_raw]
+    return rows
+
+rows = run_query("SELECT word FROM `bigquery-public-data.samples.shakespeare` LIMIT 10")
+
+# Print results.
+st.write("Some wise words from Shakespeare:")
+for row in rows:
+    st.write("✍️ " + row['word'])
  
 # Create a sample dataframe
 data = pd.DataFrame({
@@ -8,4 +33,4 @@ data = pd.DataFrame({
 })
  
 # Create a bar chart
-st.bar_chart(data)
+st.table(data)
